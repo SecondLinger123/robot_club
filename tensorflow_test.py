03cfa2135c0332a80db4f3c7fa6c77ff0e15f98f
SecondLinger123 <74628043+SecondLinger123@users.noreply.github.com>
2020-11-22 18:02:20 +0900

    tensorflow test
    tensorflow test

---
+++ tensorflow_test.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(120, 120)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
