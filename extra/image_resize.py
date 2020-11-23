1e37de12717e2bb1383d4b7f437f82d95cff334c
SecondLinger123 <74628043+SecondLinger123@users.noreply.github.com>
2020-11-23 20:28:02 +0900

    Update image_resize.py

--- extra/image_resize.py
+++ extra/image_resize.py
@@ -1,15 +1,17 @@
 import cv2
 import sys
-
+#read image
 img = cv2.imread(sys.argv[1], cv2.IMREAD_UNCHANGED)
+
+#image scale in 0-100
 scale_percent = 10 
 
-
+#determine size
 width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
 
 # resize image
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
- 
+#write
 cv2.imwrite("small"+sys.argv[1], resized) 
